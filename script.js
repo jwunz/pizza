2da27c628c9d40e3afc2a5288aa7db822822c34d
zpeterson22 <zachrp2296@yahoo.com>
2017-06-05 13:27:57 -0600

    Work on the summary page

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider){
+app.config(
+    function($scope){
+        $scope.order = {
+            pizza : [
+                {topping: "pepperonit"},
+                {topping: "green pepper"},
+                {topping: "sausage"}
+            ]
+        }
+    },
+
+    function($routeProvider){
     $routeProvider 
     //routes
         .when('/', {
-            templateUrl: 'pages/toppings.html',
+            templateUrl: 'pages/basics.html',
             controller: 'mainController',
           //  css: "../scss/main.css"
         })
         .when('/presets', {
-            templateUrl: 'pages/toppings.html',
-            controller: 'mainController',
+            templateUrl: 'pages/presets.html',
+            controller: 'presetsController',
+           // css: "../scss/main.css"    
+        })
+        .when('/summary', {
+            templateUrl: 'pages/summary.html',
+            controller: 'summaryController',
            // css: "../scss/main.css"    
         })
 });
 
 app.controller('mainController', function($scope){
+    $scope.title = "";
+});
+app.controller('summaryController', function($scope){
+    $scope.title = "Summary";
 });
