48c04cd33e05e1401f97a4425989f22cf32e84ce
Tasokun <tasokun@gmail.com>
2017-06-05 13:25:56 -0600

    Pushing radiobuttons

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ app.config(function($routeProvider){
 
 app.controller('mainController', function($scope){
 });
+
+function radioclicked(radioid, radioimageid, imagesrc){
+    turnoffradio();
+    if (document.getElementById(radioid).checked) 
+        {
+            document.getElementById(radioimageid).src = imagesrc + "filled.png";
+        }
+}
+
+function turnoffradio(){
+    document.getElementById("radio1image").src = "../images/radioleft.png";
+    document.getElementById("radio2image").src = "../images/radioboth.png";
+    document.getElementById("radio3image").src = "../images/radioright.png";
+}
+
+function toggleExtras(){
+    if(document.getElementById('extras').checked){
+       document.getElementById('extrasImage').src = "../images/extrasfilled.png"
+    }else{
+       document.getElementById('extrasImage').src = "../images/extras.png" 
+    }
+}
